ad77a1483a7f0251dc361cd67618285dfb053c95
Stephen Tellis <tellisstephen@gmail.com>
2022-05-17 19:45:38 +0200

    EX 3 added

---
+++ src/ex_3/smb_highlevel_controller/include/smb_highlevel_controller/SmbHighlevelController.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <ros/ros.h>
+
+// msgs
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/Twist.h>
+
+// STD
+#include <string>
+
+namespace smb_highlevel_controller {
+
+/*!
+ * Class containing the Husky Highlevel Controller
+ */
+class SmbHighlevelController {
+public:
+	/*!
+	 * Constructor.
+	 */
+	SmbHighlevelController(ros::NodeHandle& nodeHandle);
+
+	/*!
+	 * Destructor.
+	 */
+	virtual ~SmbHighlevelController();
+
+	bool readParameters();
+
+	void topicCallback(const sensor_msgs::LaserScan& message);
+
+private:
+	ros::NodeHandle nodeHandle_;
+	ros::Subscriber subscriber_;
+	std::string subscriberTopic_;
+	int queueSize_;
+	double pgain_angular_;
+	double pgain_linear_;
+	double angle_min_;
+	double angle_inc_;
+	bool alternate_ = true;
+	ros::Publisher publisher_;
+};
+
+} /* namespace */
---
+++ src/ex_3/smb_highlevel_controller/src/SmbHighlevelController.cpp
@@ -0,0 +1,72 @@
+#include <smb_highlevel_controller/SmbHighlevelController.hpp>
+
+namespace smb_highlevel_controller {
+
+SmbHighlevelController::SmbHighlevelController(ros::NodeHandle& nodeHandle) :
+  nodeHandle_(nodeHandle)
+{
+    if (!readParameters()) {
+    ROS_ERROR("Could not read parameters.");
+    ros::requestShutdown();
+  }
+  subscriber_ = nodeHandle_.subscribe(subscriberTopic_, queueSize_,
+                                      &SmbHighlevelController::topicCallback, this);
+  
+  publisher_ = nodeHandle_.advertise<geometry_msgs::Twist> ("/cmd_vel", 10);
+
+  ROS_INFO("Successfully launched node.");
+}
+
+SmbHighlevelController::~SmbHighlevelController()
+{
+}
+
+bool SmbHighlevelController::readParameters()
+{
+  if (!nodeHandle_.getParam("subscriber_topic", subscriberTopic_)) return false;
+  if (!nodeHandle_.getParam("queue_size", queueSize_)) return false;
+  if (!nodeHandle_.getParam("K_p_angular", pgain_angular_)) return false;
+  if (!nodeHandle_.getParam("K_p_linear", pgain_linear_)) return false;
+  if (!nodeHandle_.getParam("/pointcloud_to_laserscan/angle_min", angle_min_)) return false;
+  if (!nodeHandle_.getParam("/pointcloud_to_laserscan/angle_increment", angle_inc_)) return false;
+  return true;
+}
+
+void SmbHighlevelController::topicCallback(const sensor_msgs::LaserScan& message)
+{
+  int size = message.ranges.size();
+  int minIndex = 0;
+  int maxIndex = size; 
+  int closestIndex = -1;
+  double minVal = 999;
+  int mindist_index = 0;
+
+  // Determine distance to pillar
+  for (int i = minIndex; i < maxIndex; i++)
+  {
+      if ((message.ranges[i] <= minVal) && (message.ranges[i] >= message.range_min) && (message.ranges[i] <= message.range_max))
+      {
+          minVal = message.ranges[i];
+          mindist_index = i;
+      }
+  }
+
+  // Determine orientation to pillar
+  double orientation_control = 0;
+  orientation_control = angle_min_ + angle_inc_ * mindist_index;
+
+  // Control 
+  geometry_msgs::Twist msg;
+  if (alternate_){
+  msg.angular.z = orientation_control * pgain_angular_;
+  alternate_ = false;
+  } else {
+  msg.linear.x = minVal * pgain_linear_;
+  alternate_ = true;
+  }
+  ROS_INFO_STREAM("Sending command: " << "linear=" << msg.linear.x << " angular=" << msg.angular.z);
+  publisher_.publish(msg);
+
+}
+
+} /* namespace */
